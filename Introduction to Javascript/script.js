f123ed53a0b512ca2cc10eb20df20935abecc5d7
Saikat Bhattacharjee <bhattacharjee.saikat3@gmail.com>
2017-11-23 18:16:20 +0530

    Add files via upload

---
+++ Introduction to Javascript/script.js
@@ -0,0 +1,6 @@
+var fname = prompt("Please enter your first name");
+var lname = prompt("Please enter your last name");
+var age = prompt("Please enter your age");
+alert("Nice to meet you, "+fname+ " "+lname);
+console.log("Your full name is "+fname+" "+lname);
+console.log("Your age is "+age);
